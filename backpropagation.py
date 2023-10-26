fa040da1dcb1799d68df61cbb96f0ce9a2f76117
mariano moura <mariano8fjfk@gmail.com>
2023-10-26 18:32:24 -0300

    move some utils logic to a object oriented paradigm

--- backpropagation.py
+++ backpropagation.py
@@ -4,3 +4,6 @@ import utils
 dataset = "dadosmamografia.csv"
 data = utils.load_csv(dataset)
 utils.min_max_normalize(data)
+
+neuron = utils.neuron([0.5, 0, 4, 9], +3, utils.MyEnum.sigmoide, 1);
+print(neuron.net([1, 1, 1, 1]), neuron.activation([1, 1, 1, 1]))
--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 ''' ----- Backpropagation code ------'''
 import numpy as np
 import pandas as pd
- 
+from enum import Enum
+
 # Load dataset file
 def load_csv(filename):
     data = pd.read_csv(filename, sep=';')
@@ -22,15 +23,6 @@ def accuracy_metric(actual, predicted):
     if actual[i] == predicted[i]:
         correct += 1
  return correct / float(len(actual)) 
-
-
-# Activation is a function that calculates the output of the node 
-# based on its inputs and the weights on individual inputs. 
-def activate(weights, inputs):
-    activation = weights[-1] #bias 
-    for i in range(len(weights)-1):
-        activation += weights[i] * inputs[i]
-    return activation
  
 # Linear neuron activation
 def linear(x):
@@ -47,3 +39,50 @@ def forward_propagate(network, row):
         new_inputs.append(neuron['output'])
         inputs = new_inputs
     return inputs
+
+# Neuron function activation
+class MyEnum(Enum):
+    def linear(x, p=1):
+        return p*x
+    
+    def sigmoide(x, p=1):
+        return (1/(1+np.exp(-1*x*p)))
+
+    def hiperbolica(x, p=1):
+        return ((1-np.exp(-1*x*p))/(1+np.exp(-1*x*p)))
+
+    def arctan(x, p=1):
+        return (p*(1/(1+x^2)))
+
+# The neuron class
+class neuron:
+    bias = 0
+    weights = []
+    activationFunctionAngularFactor = 1
+    def activationFunction(): pass
+    
+    # Neuron contructor, it receives, 4 arguments, thus, they are described below.
+    # weights: indicates the neuron link weights
+    # bias: bias of the neuron
+    # activationFunction: define the activation function of the neuron
+    # activationFunctionAngularFactor: it is the angular factor of the neuron
+    def __init__(self, weights, bias, activationFunction, activationFunctionAngularFactor):
+        self.weights = weights
+        self.bias = bias
+        self.activationFunction = activationFunction
+        self.activationFunctionAngularFactor = activationFunctionAngularFactor
+
+    # net is a function that calculates the output of the node 
+    # based on its inputs and the weights on individual inputs. 
+    def net(self, inputs):
+        activation = self.bias #bias 
+        for i in range(len(self.weights)):
+            activation += self.weights[i] * inputs[i]
+        return activation
+    
+    # Activation is a function that calculates the output of the neuron 
+    # based on its net and activation function.
+    def activation(self, inputs):
+        net = self.net(inputs)
+        return self.activationFunction(net, self.activationFunctionAngularFactor)
+ 
